98f675e561039f4fa85ed5bcdf15d05f8e7393cf
Alex Silva <sacuraba@yahoo.com.br>
2020-12-03 15:22:00 -0300

    modificações

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.nirvana);
 
         inicializarSeakbar();
 
